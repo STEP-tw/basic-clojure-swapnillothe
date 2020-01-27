9c985876ea687dc1d19839f5cf81f96545397e68
Swapnil Lothe <lothe.swapnil@metronom-external.com>
2020-01-27 14:55:48 +0530

    Implemented informative-division function

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -7,3 +7,9 @@
     (is (= 2 (safe-divide 4 2))))
   (testing "zero denominator"
     (is (nil? (safe-divide 3 0)))))
+
+(deftest informative-division
+  (testing "non zero denominator"
+    (is (= 2 (informative-divide 4 2))))
+  (testing "zero denominator"
+    (is (= :infinite (informative-divide 3 0)))))
